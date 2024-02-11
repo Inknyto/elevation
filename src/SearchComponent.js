7b315fb1735857b821a94e934a69c3119293b7ab
Inknyto <nytosene@gmail.com>
2024-02-11 01:26:08 +0000

    added parsed skills

--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, CardHeader,Input, CardActions, Button, Typography, OutlinedInput } from '@mui/material';
+import { Card, CardContent, CardHeader, Input, CardActions, Button, Typography, OutlinedInput } from '@mui/material';
 import LetterComponent from './LetterComponent';
 import './style.css';
 import { credentials } from './credentials';
@@ -13,6 +13,12 @@ const SearchComponent = () => {
   const [blurrerVisible, setBlurrerVisible] = useState(false);
   const [selectedResult, setSelectedResult] = useState(null);
   const [isFixed, setIsFixed] = useState(false);
+  const [letterFormData, setLetterFormData] = useState({
+    from: '',
+    to: '',
+    subject: '',
+    body: '',
+  });
 
   const handleSearchInputChange = async (event) => {
     const query = event.target.value;
@@ -64,141 +70,108 @@ const SearchComponent = () => {
     setBlurrerVisible(false);
   };
 
-const CardComponent = ({ title, company_name, location, via, description, onCloseClick }) => {
-  return (
-    <div className="modal">
-      <Card variant="outlined" className="job-card">
-        <CardContent>
-          <CardHeader title={title} subheader={`${company_name}, ${location}`} />
-          <Typography>
-            <Typography component="span" variant="body2" color="textSecondary">
-              <strong>Via:</strong> {via}
-            </Typography><br/>
-            <div className="scrollable-text">
-            <Typography component="span" variant="body2" color="textSecondary">
-              {description}
+  const handleSendLetter = () => {
+  };
+  const handleRemoveLetterDiv = () => {
+  };
+
+
+  const CardComponent = ({ title, company_name, location, via, description, onCloseClick }) => {
+    return (
+      <div className="modal">
+        <Card variant="outlined" className="job-card">
+          <CardContent>
+            <CardHeader title={title} subheader={`${company_name}, ${location}`} />
+            <Typography component={'span'} >
+              <Typography component="span" variant="body2" color="textSecondary">
+                <strong>Via:</strong> {via}
+              </Typography><br />
+              <div className="scrollable-text">
+                <Typography component="span" variant="body2" color="textSecondary">
+                  {description}
+                </Typography>
+              </div>
             </Typography>
-            </div>
-          </Typography>
-          <CardActions>
-            <Button variant="outlined" onClick={closeBlurrer}>
-              Close
-            </Button>
-          </CardActions>
-        </CardContent>
-      </Card>
-    </div>
-  );
-};
-
-//const CardComponent = ({ title, company_name, location, via, description, onCloseClick }) => {
-//  return (
-//    <div className="modal">
-//      <Card variant="outlined" className="job-card">
-//        <CardContent>
-//          <CardHeader title={title} subheader={`${company_name}, ${location}`} />
-//          <Typography>
-//            <p><strong>Via:</strong> {via}</p>
-//            <p>{description}</p>
-//          </Typography>
-//          <CardActions>
-//            <Button variant="outlined" onClick={onCloseClick}>
-//              Close
-//            </Button>
-//          </CardActions>
-//        </CardContent>
-//      </Card>
-//    </div>
-//  );
-//};
+            <CardActions>
+              <Button variant="outlined" onClick={() => setShowLetterComponent(!showLetterComponent)}>
+                Apply
+              </Button>
+            </CardActions>
+            <CardActions>
+              <Button variant="outlined" onClick={closeBlurrer}>
+                Close
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  };
 
   return (
-      <div id='search-engine-wrapper'>
-    <OutlinedInput
-    // variant='outlined'
-      id='search-input'
-      placeholder='What job are you looking for?'
-      type="text"
-      value={searchInputValue}
-      onChange={handleSearchInputChange}
-    />
-    <div id='search-results'>
-      {searchResults.map(result => (
-        <div key={result.id}>
-          {/* Display search results */}
-          <Typography variant="h6">Title: {result.title}</Typography>
-          <Typography variant="body1">Company name: {result.company_name}</Typography>
-          <Typography variant="body1">Location: {result.location}</Typography>
-          <Typography variant="body2">Via: {result.via}</Typography>
-          {/* Uncomment the following line if you want to display the description */}
-          {/* <Typography variant="body2">Description: {result.description}</Typography> */}
-          <Button variant="outlined" onClick={() => showMore(result.id)}>
-            More
-          </Button>
-        </div>
-      ))}
-    </div>
-    {blurrerVisible && (
-      <CardComponent
-        title={selectedResult.title}
-        company_name={selectedResult.company_name}
-        location={selectedResult.location}
-        via={selectedResult.via}
-        description={selectedResult.description}
+    <div id='search-engine-wrapper'>
+      <OutlinedInput
+        // variant='outlined'
+        id='search-input'
+        placeholder='What job are you looking for?'
+        type="text"
+        value={searchInputValue}
+        onChange={handleSearchInputChange}
       />
-    )}
-  </div>
+      <div id='search-results'>
+        {searchResults.map(result => (
+          <div className='result-div' key={result.id}>
+		<div className='result-data'>
+            {/* Display search results */}
+            <Typography variant="h6">Title: {result.title}</Typography>
+            <Typography variant="body1">Company name: {result.company_name}</Typography>
+            <Typography variant="body1">Location: {result.location}</Typography>
+            <Typography variant="body2">Via: {result.via}</Typography>
+            {/* Uncomment the following line if you want to display the description */}
+            {/* <Typography variant="body2">Description: {result.description}</Typography> */}
+            <Button variant="outlined" onClick={() => showMore(result.id)}>
+              More
+            </Button>
+		</div>
+            <div key={result.id+'skills'} className="job_skills">
+              <h3>Required_skills:</h3>
 
-//     <Typography id='search-engine-wrapper'>
-//       <input 
-//       id='searchInput' 
-//       placeholder='What job are you looking for?'
-//       type="text"
-//       value={searchInputValue} 
-//       onChange={handleSearchInputChange} />
-//         <div id='search-results'>
-//     {searchResults.map(result => (
-//       <Typography key={result.id}>
-//         {/* Display search results */}
-//         <Typography variant="h6">Title: {result.title}</Typography>
-//         <Typography variant="body1">Company name: {result.company_name}</Typography>
-//         <Typography variant="body1">Location: {result.location}</Typography>
-//         <Typography variant="body2">Via: {result.via}</Typography>
-//         {/* Uncomment the following line if you want to display the description */}
-//         {/* <Typography variant="body2">Description: {result.description}</Typography> */}
-//         <Button variant="outlined" onClick={() => showMore(result.id)}>
-//           More
-//         </Button>
-//       </Typography>
-//     ))}
-//   </div>
-// {/* 
-//     <div id='search-results'>
-//       {searchResults.map(result => (
-//         <div key={result.id}>
-//           {/* Display search results */}
-//           {/* <p>Title: {result.title}</p>
-//           <p>Company name: {result.company_name}</p>
-//           <p>Location: {result.location}</p>
-//           <p>Via: {result.via}</p>
-//           {/* <p>Description: {result.description}</p> */}
-//           {/* <Button variant="outlined" onClick={() => showMore(result.id)}>
-//             More
-//           </Button>
-//         </div>
-//       ))} */}
-//         {/* </div> */} 
-  
-//       {blurrerVisible && (
-//         <CardComponent
-//           title={selectedResult.title}
-//           company_name={selectedResult.company_name}
-//           location={selectedResult.location}
-//           via={selectedResult.via}
-//           description={selectedResult.description}
-//         />
-//       )}
-//     </Typography>
+              {Object.entries(result.skills).map(([category, skills]) => (
+                <div key={category} className="skill_category">
+                  <span className="category_title">{category}</span>
+
+                  {skills.map((skill, skillIndex) => (
+                    <span key={skillIndex} className="skill">
+                      {skill}
+                    </span>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      {blurrerVisible && (
+        <CardComponent
+          title={selectedResult.title}
+          company_name={selectedResult.company_name}
+          location={selectedResult.location}
+          via={selectedResult.via}
+          description={selectedResult.description}
+        />
+      )}
+      {showLetterComponent && (
+        <LetterComponent
+          letterFormData={letterFormData}
+          setLetterFormData={setLetterFormData}
+          onSendLetter={handleSendLetter}
+          onRemoveLetterDiv={handleRemoveLetterDiv}
+        />
+      )}
+
+    </div>
+
+    //     </Typography>
   );
 };
 
