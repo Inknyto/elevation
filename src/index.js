a9be983e0bc707b3397e88e8c1d43b20608846ce
Inknyto <nytosene@gmail.com>
2024-02-15 18:54:16 +0000

    added unconfigured dark mode

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { useContext } from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { darkMode } from './Icons';
-import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material/styles';
+import { DarkModeProvider, DarkModeContext } from './DarkMode';
 import { lightTheme, darkTheme } from './Theme';
-// import { toggleDarkMode } from './Icons';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-    //<ThemeProvider theme={ toggleDarkMode? darkTheme : lightTheme }>
-    //<ThemeProvider theme={ lightTheme }>
-root.render(
-<React.StrictMode>
-    <ThemeProvider theme={ darkMode == 1? darkTheme : lightTheme }>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function AppWithTheme() {
+  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <App handleDarkModeToggle={toggleDarkMode} />
+    </ThemeProvider>
+  );
+}
+
+root.render(
+  <DarkModeProvider>
+    <React.StrictMode>
+      <AppWithTheme />
+    </React.StrictMode>
+  </DarkModeProvider>
+);
